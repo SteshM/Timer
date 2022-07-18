1a40bb95e27e31ad6bcac5629a61d32bd1d9463c
Stella Mbithe <mbithestella0@gmail.com>
2022-07-18 23:40:57 +0300

    Fixed timer not printing

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print("'\r{0}".format(timer), end='')
         time.sleep(1)
         t -= 1
 
-    print('Timer Completed!')
+    print('\nTimer Completed!')
 
 
 t = input('Enter the time in seconds: ')
